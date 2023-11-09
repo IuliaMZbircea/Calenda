7669961cfbd7ebfe30bce71c0bab6b7045980e31
alexandragulpe <gulpe.alexandra@yahoo.com>
2023-11-10 01:05:26 +0200

    Start Page + First Page

--- app/index.js
+++ app/index.js
@@ -1,11 +1,43 @@
-import { View, Text} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import CustomButton from './components/CustomButton';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+    const navigation = useNavigation();
+    
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Text style={styles.welcomeText}>Welcome to Calenda</Text>
+            <Text style={styles.subText}>A new way to organize your time efficiently.</Text>
+            <CustomButton
+                title="Get Started"
+                onPress={() => navigation.navigate('NextPage')} 
+                
+            />
         </View>
-    )
-}
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#fff', 
+    },
+    welcomeText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
+    subText: {
+        fontSize: 16,
+        marginBottom: 32,
+    },
+   
+});
 
 export default Home;
