bf58b8f445731ccf86f59ad93cae678ca8c9adf2
alexandragulpe <gulpe.alexandra@yahoo.com>
2023-11-14 15:14:37 +0200

    CreateTaskPage Design

--- app/CreateTaskPage.js
+++ app/CreateTaskPage.js
@@ -1,12 +1,96 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
 
 const CreateTaskPage = () => {
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [description, setDescription] = useState('');
+
+  const createTask = () => {
+    console.log('Task Created', { title, date, time, description });
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Create Task Page</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Create task</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Title"
+        value={title}
+        onChangeText={setTitle}
+      />
+      <View style={styles.dateTimeContainer}>
+        <TextInput
+          style={[styles.input, styles.dateInput]}
+          placeholder="Date"
+          value={date}
+          onChangeText={setDate}
+        />
+        <TextInput
+          style={[styles.input, styles.timeInput]}
+          placeholder="Time"
+          value={time}
+          onChangeText={setTime}
+        />
+      </View>
+      <TextInput
+        style={[styles.input, styles.descriptionInput]}
+        placeholder="Description"
+        value={description}
+        onChangeText={setDescription}
+        multiline
+      />
+      <TouchableOpacity style={styles.button} onPress={createTask}>
+        <Text style={styles.buttonText}>Create</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'white',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+  dateTimeContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  dateInput: {
+    flex: 2,
+    marginRight: 10,
+  },
+  timeInput: {
+    flex: 1,
+  },
+  descriptionInput: {
+    height: 100,
+    textAlignVertical: 'top',
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
 export default CreateTaskPage;
