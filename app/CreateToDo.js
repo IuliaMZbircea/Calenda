35ff6ffbe46c38a1fb78364b20f32d7aefee6b12
alexandragulpe <gulpe.alexandra@yahoo.com>
2024-01-10 18:01:25 +0200

    to do list modifications: icons, mark as done, delete todo

--- app/CreateToDo.js
+++ app/CreateToDo.js
@@ -1,27 +1,80 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Button, Text } from 'react-native';
 import { FIRESTORE_DB } from '../FirebaseConfig';
-import { addDoc, collection } from 'firebase/firestore';
-import { TextInput } from 'react-native-gesture-handler';
-
+import { addDoc, collection, onSnapshot, updateDoc, doc, deleteDoc } from 'firebase/firestore';
+import { FlatList, TextInput} from 'react-native-gesture-handler';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Entypo } from '@expo/vector-icons';
 
 const List = ({}) => {
 
     const [todos, setTodos] = useState([]);
     const [todo, setTodo] = useState('');
 
-    useEffect(() => { }, []);
+    useEffect(() => {
+        const todoRef = collection(FIRESTORE_DB, 'todos');
+
+        const subscriber = onSnapshot(todoRef, {
+            next: (snapshot) => {
+                
+                const todos = [];
+                snapshot.docs.forEach((doc) => {
+                    todos.push({
+                        id: doc.id,
+                        ...doc.data(),
+                    });
+                });
+                setTodos(todos);
+            },
+        });
+        return () => subscriber();
+     }, []);
 
     const addTodo = async () => {
         const doc = await addDoc(collection(FIRESTORE_DB, 'todos'), { title: todo, done: false });
         setTodo = '';
     };
+    
+    const renderTodo = ({item}) => {
+        const ref = doc(FIRESTORE_DB, `todos/${item.id}` )
+
+        const toggleDone = async() => {
+            updateDoc(ref, { done: !item.done});
+
+        }
+
+        const deleteItem = async() => {
+            deleteDoc(ref);
+            
+        }
+        return (
+            <View style={styles.todoContainer}>
+               <TouchableOpacity onPress={toggleDone} style={styles.todo}>
+                {item.done && <Ionicons name="checkmark-circle-outline" size={32} color="green" />}
+                {!item.done && <Entypo name="circle" size={32} color="black" />}
+
+                <Text style={styles.todoText}>{item.title}</Text>
+               </TouchableOpacity>
+               <Ionicons name="trash-bin-outline" size={24} color="red" on onPress={deleteItem} />
+            </View>
+        )
+    }
     return (
         <View style={styles.container}>
             <View style = {styles.form}>
             <TextInput style = {styles.input} placeholder = 'Add new todo' onChangeText={(text) => setTodo(text)} value={todo}/>
             <Button onPress={addTodo} title = 'Add Todo' disabled = {todo === ''}/>
             </View>
+            { todos.length > 0 && (
+                <View>
+                <FlatList
+                data={todos}
+                renderItem={(item) => renderTodo(item)}
+                keyExtractor={(todo) => todo.id}/>
+
+             </View>
+            )}
+            
         </View>
     );
 };
@@ -44,6 +97,23 @@ const styles = StyleSheet.create({
         borderRadius: 3,
         padding: 10,
         backgroundColor: '#fff',
+    },
+    todoContainer:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#ededed',
+        padding: 10,
+        marginVertical: 4,
+    },
+    todoText: {
+        flex:1,
+        paddingHorizontal: 4
+
+    },
+    todo:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
     }
 });
 
